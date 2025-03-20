75e36edc95062a509ac9c5cf82522b4b1300a87d
parthgoyal13 <nparthgoyal.skncoe.comp@gmail.com>
2025-03-20 21:19:49 +0530

    added app.put routes to edit book data

--- index.js
+++ index.js
@@ -55,6 +55,25 @@ app.delete("/books/:id", async (req, res) => {
   }
 });
 
+app.put("/books/:id", async (req, res) => {
+  const bookId = req.params.id;
+  const { title, author, genre } = req.body;
+  try {
+    const updatedBook = await Books.findByIdAndUpdate(
+      bookId,
+      { title, author, genre },
+      { new: true }
+    );
+    if (!updatedBook) {
+      return res.status(404).json({ error: "Book not found" });
+    }
+    res.status(200).json(updatedBook);
+  } catch (error) {
+    console.error("Error updating book:", error);
+    res.status(500).json({ error: "Internal server error" });
+  }
+});
+
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
